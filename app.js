e9785e6ddfd99abb5de4c8d6432e8fea0fe88521
MaykonMSP <maykonpereira460@gmail.com>
2025-06-20 10:40:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+let numeroSecreto = gerarNumerosAleatorios();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+     if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
+}
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    
+
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector(`input`).value;
+    console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas': 'tentativa';
+                let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`; 
+                exibirTextoNaTela('p', mensagemTentativas);
+                document.getElementById('reiniciar').removeAttribute('disabled');
+        } else {
+                if (chute > numeroSecreto) {
+                        exibirTextoNaTela('p', 'O número secreto é menor');
+                } else {
+                        exibirTextoNaTela('p', 'O número secreto é maior');
+                }
+                tentativas++;
+                limparCampo()
+        }
+
+}
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumerosAleatorios();
+    limparCampo();
+    tentativas = 1; 
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true)
+
+
+}
+
+function gerarNumerosAleatorios(){
+    return parseInt(Math.random()  *10 + 1  );
+}
